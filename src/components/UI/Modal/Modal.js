1a929042570731480c87f82263c67cdeb6e918ba
Yash Pratap Singh <singhyashpratap06@gmail.com>
2019-04-12 15:11:45 +0530

    added sidedrawer, navigation and other modifications in toolbar, mobile first

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,20 +1,31 @@
-import React from 'react';
+import React, {Component} from 'react';
 import classes from './Modal.module.css';
-import Aux from '../../../hoc/Aux'; 
+import Aux from '../../../hoc/Aux/Aux'; 
 import Backdrop from '../../UI/Backdrop/Backdrop';
 
-const modal = (props) => (
-    <Aux>
-        <Backdrop show={props.show} clicked={props.modelClosed}/>
-        <div 
-            className={classes.Modal}
-            style={{
-                transform : props.show ? 'translateY(0)' : 'translateY(-1000vh)',
-                opacity : props.show ? 1 : 0
-            }}>
-            {props.children}
-        </div>
-    </Aux>
-)
 
-export default modal;
+class  Modal extends Component {
+    shouldComponentUpdate(nextProps, nextState) {
+        return nextProps.show !== this.props.show
+    }
+    componentWillUpdate() {
+        console.log("[Modal] will update")
+    }
+    render() {
+        return (
+            <Aux>
+                <Backdrop show={this.props.show} clicked={this.props.modelClosed}/>
+                <div 
+                    className={classes.Modal}
+                    style={{
+                        transform : this.props.show ? 'translateY(0)' : 'translateY(-1000vh)',
+                        opacity : this.props.show ? 1 : 0
+                    }}>
+                    {this.props.children}
+                </div>
+            </Aux>
+        )
+    }
+} 
+
+export default Modal;
